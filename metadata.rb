e4b135286fc93c63636876b4240ca5742d95dab5
Robert Veznaver <r.veznaver@criteo.com>
2015-01-30 19:25:16 +0100

    install build-essential at compile time

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,11 @@ maintainer_email 'r.veznaver@criteo.com'
 license          'Apache License 2.0'
 description      'Installs rundeck and configure as needed'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends          'yum'
 depends          'java'
+depends          'build-essential'
 
 suggests         'rundeck-bridge'
 suggests         'rundeck-node'
--- recipes/default.rb
+++ recipes/default.rb
@@ -11,4 +11,9 @@ service 'rundeckd' do
   action   [:enable, :start]
 end
 
+# Install build essential at compile time for gem compilation
+node.default['build-essential']['compile_time'] = true
+include_recipe 'build-essential'
+
+# Install rundeck gem for API communication
 chef_gem 'rundeck'
