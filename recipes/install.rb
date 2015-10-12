e8249790b9e7261efd20f49cae7cb0a44b34eabe
Virender Khatri <vir.khatri@gmail.com>
2015-10-13 01:52:18 +0530

    added ubuntu platform support

--- recipes/install.rb
+++ recipes/install.rb
@@ -4,17 +4,27 @@
 
 include_recipe 'java' if node['rundeck_server']['install_java']
 
-# Configure yum repo if specified
-yum_repository 'rundeck' do
-  description node['rundeck_server']['yum']['description']
-  baseurl node['rundeck_server']['yum']['baseurl']
-  gpgcheck node['rundeck_server']['yum']['gpgcheck']
-  gpgkey node['rundeck_server']['yum']['gpgkey']
-  enabled node['rundeck_server']['yum']['enabled']
-  action node['rundeck_server']['yum']['action']
+case node['platform_family']
+when 'rhel', 'fedora'
+  # Configure yum repo if specified
+  yum_repository 'rundeck' do
+    description node['rundeck_server']['yum']['description']
+    baseurl node['rundeck_server']['yum']['baseurl']
+    gpgcheck node['rundeck_server']['yum']['gpgcheck']
+    gpgkey node['rundeck_server']['yum']['gpgkey']
+    enabled node['rundeck_server']['yum']['enabled']
+    action node['rundeck_server']['yum']['action']
+  end
+when 'debian'
+  apt_repository 'rundeck' do
+    uri node['rundeck_server']['apt']['uri']
+    components node['rundeck_server']['apt']['components']
+    key node['rundeck_server']['apt']['key']
+    action node['rundeck_server']['apt']['action']
+  end
 end
 
 # Install RunDeck package
 package 'rundeck' do
-  action :install
+  options node['rundeck_server']['apt']['options'] if node['platform_family'] == 'debian'
 end
