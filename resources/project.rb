99abe9bd28220e2741d1aeb014554765eba08b30
Florian Philippon <florian.philippon@s4m.io>
2017-03-07 17:07:36 +0100

    Fix syntax errors with ruby 2.4

--- resources/project.rb
+++ resources/project.rb
@@ -53,27 +53,27 @@ attribute :name,
 attribute :executor,
           kind_of: [Symbol, Hash],
           default: :ssh,
-          callbacks: {
+          callbacks: ({
             must_contain_provider: lambda do |executor|
               executor.is_a?(Symbol) || !executor['provider'].nil? || !executor[:provider].nil?
             end,
             must_contain_config: lambda do |executor|
               executor.is_a?(Symbol) || (executor['config'] || executor[:config]).is_a?(Hash)
             end
-          }
+          })
 
 # <> @attribute sources List of node sources
 attribute :sources,
           kind_of: Array,
           required: true,
-          callbacks: {
+          callbacks: ({
             must_be_an_array_of_hashes: lambda do |sources|
               sources.all? { |source| source.is_a?(Hash) }
             end,
             must_contain_type: lambda do |sources|
               sources.all? { |source| source['type'] || source[:type] }
             end
-          }
+          })
 
 attribute :cookbook,
           kind_of: String,
