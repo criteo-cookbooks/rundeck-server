a798e8440d804b5cfcad2449f7ab0a965c620847
Erinn Looney-Triggs <erinn.looneytriggs@gmail.com>
2018-07-31 13:14:44 -0600

    Update metadata.

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,8 @@ license          'Apache-2.0'
 description      'Installs rundeck and configure as needed'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.0'
-supports         'centos', 'redhat'
+supports         'centos'
+supports         'redhat'
 
 depends          'yum'
 depends          'java'
