e6a8289fc366bb01d69b81b7b5981121b3e6ec1e
Grégoire Seux <g.seux@criteo.com>
2018-10-10 13:52:06 +0200

    Release 1.7.2
    Change-Id: Id9eef60412883073fe7cf472a0f6cbe3832646f5

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'use_github_issues@criteo.com'
 license          'Apache-2.0'
 description      'Installs rundeck and configure as needed'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.7.1'
+version          '1.7.2'
 supports         'centos'
 
 depends          'yum'
